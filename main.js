49f7d1c9d36499317ac6203aaf3929bcf508d35d
Vitoria <vivi.wsr@hotmail.com>
2022-06-15 10:59:55 -0300

    projeto javascript web

---
+++ main.js
@@ -0,0 +1,32 @@
+function tocaSom (idElementoAudio) {
+    const elemento = document.querySelector(idElementoAudio);
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+       console.log('Elemento não encontrado ou inválido');
+    }
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaDeTeclas.length; i++ ) {
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+       
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
